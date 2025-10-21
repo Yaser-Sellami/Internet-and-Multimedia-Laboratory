feb0e243224e4d98875eed6ceaf51118872c34fc
Yaser Sellami <yasersellami81@gmail.com>
2025-10-21 15:09:51 +0200

    Created the function imtran

---
+++ Introduction_to_image_processing/imtran.m
@@ -0,0 +1,17 @@
+function g = imtran(f, trasf)
+%f is the input image
+%trasf is the transformation to apply to the input image
+
+if trasf == "neg"
+    g = NegImg(f, 255);
+elseif trasf == "log"
+    g = LogImg(f, 1);
+elseif trasf == "pot"
+    g = PotImg(f, 0, 1, 2);
+elseif trasf == "con"
+    g = ConImg(f, 0.5, 20);
+else 
+    print("Transformation function not valid")
+end
+
+end
