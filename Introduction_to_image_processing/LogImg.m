5fd5e6abd6b4bb1c887b6de2c4f5ca1785fc4023
Yaser Sellami <yasersellami81@gmail.com>
2025-11-04 15:24:53 +0100

    Updated input description

--- Introduction_to_image_processing/LogImg.m
+++ Introduction_to_image_processing/LogImg.m
@@ -1,5 +1,8 @@
 function g = LogImg(f, c)
+% Input:
+% f: Input image
+% c: Amplitude constant
 
-g = c * log(1 + double(f));
+g = c*log(1+double(f));
 
 end
--- Introduction_to_image_processing/NegImg.m
+++ Introduction_to_image_processing/NegImg.m
@@ -1,4 +1,7 @@
 function g = NegImg(f,L)
+% Input:
+% f: Input image
+% L: Gray levels (256 in our case)
 
 g = L-1-f;
 
