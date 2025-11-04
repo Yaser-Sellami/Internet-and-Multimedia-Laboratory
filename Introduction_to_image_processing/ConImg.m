39884caa12c6446c734ff10941b3ad3ed4c07aab
Yaser Sellami <yasersellami81@gmail.com>
2025-11-04 15:40:21 +0100

    Updating input description

--- Introduction_to_image_processing/ConImg.m
+++ Introduction_to_image_processing/ConImg.m
@@ -1,6 +1,9 @@
 function g = ConImg(f,m,E)
-%f = input image
-%m = slope
-%E = dark-light switch
+% Input:
+% f: Input image
+% m: Slope
+% E: Dark-Light switch
+
 g = 1./(1+(m./(double(f)+eps)).^E);
+
 end
