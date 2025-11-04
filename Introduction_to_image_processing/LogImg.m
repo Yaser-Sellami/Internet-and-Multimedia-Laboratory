b2d5303da83c6afd31f69f4dd9471d3f7e2ec5fc
Yaser Sellami <yasersellami81@gmail.com>
2025-11-04 16:19:16 +0100

    Removing standardization

--- Introduction_to_image_processing/LogImg.m
+++ Introduction_to_image_processing/LogImg.m
@@ -3,9 +3,6 @@ function g = LogImg(f, c)
 % f: Input image
 % c: Amplitude constant
 
-% Standardizing the image so that the effect of c does not vanish
-f = f/max(max(f));
-
 g = c*log(1+double(f));
 
 end
