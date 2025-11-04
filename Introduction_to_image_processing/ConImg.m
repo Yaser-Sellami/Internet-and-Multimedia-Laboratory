66e1fb84f5e09ea295ec95a2a38d2e278061e591
Yaser Sellami <yasersellami81@gmail.com>
2025-11-04 16:13:07 +0100

    Adding the standardization

--- Introduction_to_image_processing/ConImg.m
+++ Introduction_to_image_processing/ConImg.m
@@ -4,6 +4,9 @@ function g = ConImg(f,m,E)
 % m: Slope
 % E: Dark-Light switch
 
+% Standardizing f in [0,1] to prevent a full-white image (since m << 255)
+f = f/max(max(f));
+
 g = 1./(1+(m./(double(f)+eps)).^E);
 
 end
