565eff5077d6fdb992b15b75edb926f820e21feb
Yaser Sellami <yasersellami81@gmail.com>
2025-11-04 16:16:36 +0100

    Adding standardization

--- Introduction_to_image_processing/LogImg.m
+++ Introduction_to_image_processing/LogImg.m
@@ -3,6 +3,9 @@ function g = LogImg(f, c)
 % f: Input image
 % c: Amplitude constant
 
+% Standardizing the image so that the effect of c does not vanish
+f = f/max(max(f));
+
 g = c*log(1+double(f));
 
 end
