b89f58fac27dee8e0942ec7c1c1a8ec49613f37e
Yaser Sellami <yasersellami81@gmail.com>
2025-11-04 16:07:49 +0100

    Correcting the research of low_in, high_in

--- Introduction_to_image_processing/PotImg.m
+++ Introduction_to_image_processing/PotImg.m
@@ -5,13 +5,12 @@ function g = PotImg(f,low_out, high_out, gamma)
 % high_out: Maximum gray level of the output image
 % gamma: Power of the function
 
-% Finding the minimum and maximum gray level of the input image
-% Dividing by the maximum gray level in general (256 levels of gray)
-% in order to standardize [low_in, low_out] between [0,1] as required by
-% the function imadjust
+% Standardizing the input image between [0,1] and
+% finding low_in, low_out between [0,1] as required by imadjust
+f = f/max(max(f));
 
-low_in = min(min(f))/255; % Minimum gray level 
-high_in = max(max(f))/255; % Maximum gray level
+low_in = min(min(f)); % Minimum gray level 
+high_in = max(max(f)); % Maximum gray level
 
 g = imadjust(f, [low_in, high_in], [low_out, high_out], gamma);
 
