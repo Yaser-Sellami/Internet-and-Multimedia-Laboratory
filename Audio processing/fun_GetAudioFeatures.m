50681aabf60aaf7447bc6335e1c1e84b09fc3e12
Yaser Sellami <yasersellami81@gmail.com>
2025-11-19 13:45:37 +0100

    Creating the function to extract the features from the audio file

--- Audio processing/fun_GetAudioFeatures.m
+++ Audio processing/fun_GetAudioFeatures.m
@@ -4,10 +4,34 @@ function F = fun_GetAudioFeatures(chunk,Nfeatures)
 % Nfeature: the number of features we want to extract
 
 [rows, columns] = size(chunk);
+F = zeros(Nfeatures, columns);
 
 for i = 1:columns
+    % Compute the fft of the column
+    fft_column = fft(chunk(:,i));
+    % Select only the positive frequencies
+    pos_freq = fft_column(1: (floor(rows/2)+1
     % Compute the power spectrum
-    ps = abs(fft(chunk(:,1))).^2;
-    pos_freq = 
+    power_spectrum = abs(pos_freq).^2;
+
+    % Divide the positive frequencies into subbands
+    nBands = 8;
+    dimBands = ceil(length(pos_freq)/nBands);
+    % Sum energy per subband
+    energy = zeros(nBands, 1);
+
+    for i = 1:nBands
+        bandStart = (i-1)*dimBands + 1;
+        bandEnd = (i)*dimBands;
+        % Check if we don't go out of index bounds
+        if bandEnd > length(power_spectrum)
+            bandEnd = length(power_spectrum);
+        end
+        energy(i) = sum(power_spectrum(bandStart:bandEnd));
+    end
+
+    % Pick the top Nfeatures subband energies
+    energy = sort(energy);
+    F(:, i) = energy((length(energy)-Nfeatures), length(energy))'; 
 end
 end
